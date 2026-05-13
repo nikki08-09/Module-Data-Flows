0efc982859d577fc36677d1fb59dce6f534b1c4d
Nikita <nikita.belgium@gmail.com>
2026-05-13 18:13:39 +0200

    Added function to get qty, item and total and call the function to get the result

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,18 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+function calculateTotal(order) {
+  let total = 0;
+  console.log("QTY".padEnd(7) + "ITEM".padEnd(20) + "TOTAL".padStart(5));
+  for (let i = 0; i < order.length; i++) {
+    const { itemName, quantity, unitPricePence } = order[i];
+    total += quantity * unitPricePence;
+    let row =
+      quantity.toString().padEnd(7) +
+      itemName.padEnd(20) +
+      total.toString().padStart(5);
+    console.log(row);
+  }
+  console.log("Total: " + total.toString());
+}
+calculateTotal(order);
