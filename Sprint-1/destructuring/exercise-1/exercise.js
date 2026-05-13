dde71dfb48ce2713e4b86f56b63cafbc1aad9038
Nikita <nikita.belgium@gmail.com>
2026-05-13 17:55:11 +0200

    Destructuring object personOne

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -3,10 +3,9 @@ const personOne = {
   age: 34,
   favouriteFood: "Spinach",
 };
-
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself(___________________________) {
+function introduceYourself({ name, age, favouriteFood } = personOne) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
