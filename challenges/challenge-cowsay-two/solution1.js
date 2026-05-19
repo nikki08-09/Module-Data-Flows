a2993cd8fcdb97d7cefc76f06aad4dc24c891ca1
nikki08-09 <nikita.belgium@gmail.com>
2026-05-19 15:49:06 +0200

    Created or draw cow and made cow variable Added some topline and bottomline spaces acorrding to saying and iterate i in for loop.

--- challenges/challenge-cowsay-two/solution1.js
+++ challenges/challenge-cowsay-two/solution1.js
@@ -1,5 +1,5 @@
 // =================
-// Stripped down cowsayer CLI, 
+// Stripped down cowsayer CLI,
 // no libraries
 // https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
@@ -10,20 +10,27 @@
 
 // 2. Make supplies for our speech bubble
 
-let topLine = '_';
-let bottomLine = '-';
-let saying = '';
+let topLine = "_";
+let bottomLine = "-";
+let saying = "";
 
 // 3. Make a cow that takes a string
 
 function cowsay(saying) {
-// how will you make the speech bubble contain the text?
+  saying = " " + saying + " ";
+  for (let i = 1; i < saying.length; i++) {
+    topLine += "_";
+    bottomLine += "-";
+  }
+  let cow = ` ${topLine}\n<${saying} >\n ${bottomLine}\n         \\    ^__^\n          \\   (oo)\\_______\n              (__)\\       )\\/\\ \n                 ||----w |\n                 ||     ||`;
+  console.log(cow);
+  // how will you make the speech bubble contain the text?
 
-// where will the cow picture go?
+  // where will the cow picture go?
 
-// how will you account for the parameter being empty?
-
+  // how will you account for the parameter being empty?
 }
+cowsay("hhbhbjh");
 
 //4. Pipe argument into cowsay function and return a cow
 
