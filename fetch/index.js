32cb6df5f89139da70cda36be3da9030acd50a84
Nikita <nikita.belgium@gmail.com>
2026-05-19 13:47:12 +0200

    Added another API link in other function

--- fetch/index.js
+++ fetch/index.js
@@ -21,3 +21,24 @@ async function xkcd() {
   });
 }
 xkcd();
+
+async function randomFox() {
+  let data = await fetch(`https://randomfox.ca/floof/`);
+  let jsonData = await data.json();
+  console.log(jsonData);
+  const generateButton = document.createElement("button");
+  generateButton.id = "generateFox";
+  generateButton.innerText = "Generate Random Fox";
+  document.body.appendChild(generateButton);
+  const imageDiv = document.createElement("div");
+  document.body.appendChild(imageDiv);
+  imageDiv.style.backgroundColor = "lightgray";
+  imageDiv.style.height = "400px";
+  generateButton.addEventListener("click", () => {
+    const img = document.createElement("img");
+    img.src = jsonData.image;
+    img.alt = "Random Fox";
+    imageDiv.appendChild(img);
+  });
+}
+randomFox();
