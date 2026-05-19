68b654033b73e364a4317e194528ba3bab19ba3e
Nikita <nikita.belgium@gmail.com>
2026-05-19 13:32:14 +0200

    Added condition

--- fetch/index.js
+++ fetch/index.js
@@ -1,5 +1,9 @@
 async function xkcd() {
   let data = await fetch(`https://xkcd.now.sh/?comic=latest`);
+  if (!data.ok) {
+    throw new Error(`HTTP error! status: ${data.status}`);
+  }
   let jsonData = await data.json();
   console.log(jsonData);
 }
+xkcd();
