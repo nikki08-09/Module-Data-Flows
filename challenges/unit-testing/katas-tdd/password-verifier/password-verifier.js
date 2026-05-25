c28dfc50218b1021b7f6820f79dee8aab1dac77f
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 17:55:58 +0200

    Added test for null/not null

--- challenges/unit-testing/katas-tdd/password-verifier/password-verifier.js
+++ challenges/unit-testing/katas-tdd/password-verifier/password-verifier.js
@@ -1,2 +1,10 @@
-function passwordVerifier(password) {}
+function passwordVerifier(password) {
+  if (password === undefined || password === "" || password === null)
+    return "Password rejected";
+  else if (password != null) {
+    if (password.length >= 8 && /[A-Z]/.test(password) && /\d/.test(password))
+      return "Password accepted";
+    else return "Password rejected";
+  }
+}
 module.exports = passwordVerifier;
--- challenges/unit-testing/katas-tdd/password-verifier/password-verifier.test.js
+++ challenges/unit-testing/katas-tdd/password-verifier/password-verifier.test.js
@@ -1,1 +1,5 @@
 const passwordVerifier = require("./password-verifier");
+test("Password is not null/is null", () => {
+  expect(passwordVerifier("Password12345678")).toEqual("Password accepted");
+  expect(passwordVerifier()).toEqual("Password rejected");
+});
