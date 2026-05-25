a5966c61c7de3e71811f516b311119935f223463
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 20:48:24 +0200

    Implemented the test sales

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,3 +1,12 @@
-function sales(carsSold) {}
+function sales(carsSold) {
+  let totals = { Ford: 0, "Land Rover": 0, Toyota: 0, Honda: 0 };
+  for (let car of carsSold) {
+    if (car.make === "Ford") totals.Ford += car.price;
+    else if (car.make === "Land Rover") totals["Land Rover"] += car.price;
+    else if (car.make === "Toyota") totals.Toyota += car.price;
+    else if (car.make === "Honda") totals.Honda += car.price;
+  }
+  return totals;
+}
 
 module.exports = sales;
