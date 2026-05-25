971d0dc6f6c0391e801bd34cc2859a8fcde819e2
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 17:34:27 +0200

    Added test for one number

--- challenges/unit-testing/katas-tdd/calculator/calculator.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.js
@@ -1,4 +1,17 @@
 function calculator(numbers) {
   if (numbers === "") return 0;
+  else if (numbers.length === 1 && isNumber(numbers)) return Number(numbers);
 }
 module.exports = calculator;
+
+function isNumber(numbers) {
+  let yes = true;
+  let arr = numbers.split(",");
+
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = Number(arr[i]);
+
+    if (Number.isNaN(arr[i])) yes = false;
+  }
+  return yes;
+}
--- challenges/unit-testing/katas-tdd/calculator/calculator.test.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.test.js
@@ -2,3 +2,6 @@ const calculator = require("./calculator");
 test("Returns zero for empty string", () => {
   expect(calculator("")).toEqual(0);
 });
+test("Returns a number if passed without another", () => {
+  expect(calculator("5")).toEqual(5);
+});
