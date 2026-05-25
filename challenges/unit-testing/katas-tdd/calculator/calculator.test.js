9f114936c336758e68704f64211fe488b5a204f0
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 17:41:03 +0200

    Add test to ignore negative numbers

--- challenges/unit-testing/katas-tdd/calculator/calculator.test.js
+++ challenges/unit-testing/katas-tdd/calculator/calculator.test.js
@@ -16,3 +16,6 @@ test("Throw an error for all the invalid inputs", () => {
     calculator("a,b,c");
   }).toThrow("Invalid input");
 });
+test("Ignore big numbers", () => {
+  expect(calculator("2,4,1,2,4,5,6,1001")).toEqual(24);
+});
