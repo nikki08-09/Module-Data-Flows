40c42cb5007b982f425c0658a6ff253e9f108844
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 21:42:39 +0200

    Implemented the function average

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,12 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  let average = 0;
+  numbers = numbers.filter((num) => typeof num === "number");
+  console.log(numbers);
+  for (let num of numbers) average += num;
+  return average / numbers.length;
+}
 
 module.exports = average;
