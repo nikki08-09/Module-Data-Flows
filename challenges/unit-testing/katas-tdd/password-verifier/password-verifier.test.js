1f7daa423c55055ddf295418395b66a44791b16b
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 17:56:24 +0200

    Added test for 8 or more characters

--- challenges/unit-testing/katas-tdd/password-verifier/password-verifier.test.js
+++ challenges/unit-testing/katas-tdd/password-verifier/password-verifier.test.js
@@ -3,3 +3,7 @@ test("Password is not null/is null", () => {
   expect(passwordVerifier("Password12345678")).toEqual("Password accepted");
   expect(passwordVerifier()).toEqual("Password rejected");
 });
+test("Password is 8 characters or more", () => {
+  expect(verifier("Mypassword1234")).toEqual("Password accepted");
+  expect(verifier("1234")).toEqual("Password rejected");
+});
