c79a78725788e4d140fde64607c0f0a5d10a0e7a
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 13:07:47 +0200

    Added some variables and Css styles  and img src.

--- challenges/dog-photo-gallery/index.js
+++ challenges/dog-photo-gallery/index.js
@@ -4,4 +4,16 @@ async function generate() {
     .catch((error) => {
       throw new Error("Error");
     });
+  img.src = image.message;
 }
+
+const btn = document.getElementById("generate-btn");
+const list = document.getElementById("list");
+const img = document.createElement("img");
+img.style.border = "2px solid black";
+img.style.height = "100%";
+img.style.width = "600px";
+list.append(img);
+list.style.height = "300px";
+list.style.width = "600px";
+btn.addEventListener("click", generate);
