a7aef1226d82fd43387322171592da113958ed3d
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 20:34:36 +0200

    Added the test for 1,5,10,50,100,500,1000

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,5 +1,12 @@
 function convertToOldRoman(n) {
   if (n === null || n === undefined || n === "" || isNaN(Number(n))) return "";
+  else if (n === "1") return "I";
+  else if (n === "5") return "V";
+  else if (n === "10") return "X";
+  else if (n === "50") return "L";
+  else if (n === "100") return "C";
+  else if (n === "500") return "D";
+  else if (n === "1000") return "M";
 }
 
 module.exports = convertToOldRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -5,3 +5,26 @@ test("Test for empty,null,undefined", () => {
   expect(convertToOldRoman()).toEqual("");
   expect(convertToOldRoman("1FVGBHN")).toEqual("");
 });
+
+test("returns V if passed 1 as an argument", function () {
+  expect(convertToOldRoman("5")).toEqual("V");
+});
+
+test("returns X if passed 1 as an argument", function () {
+  expect(convertToOldRoman("10")).toEqual("X");
+});
+
+test("returns L if passed 1 as an argument", function () {
+  expect(convertToOldRoman("50")).toEqual("L");
+});
+test("returns C if passed 1 as an argument", function () {
+  expect(convertToOldRoman("100")).toEqual("C");
+});
+
+test("returns D if passed 1 as an argument", function () {
+  expect(convertToOldRoman("500")).toEqual("D");
+});
+
+test("returns M if passed 1 as an argument", function () {
+  expect(convertToOldRoman("1000")).toEqual("M");
+});
