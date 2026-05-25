4e155ae76192a4a48af236d8e03b85f6b905e30e
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 17:57:59 +0200

    Added test for upper case included and if password has a number

--- challenges/unit-testing/katas-tdd/password-verifier/password-verifier.test.js
+++ challenges/unit-testing/katas-tdd/password-verifier/password-verifier.test.js
@@ -7,3 +7,12 @@ test("Password is 8 characters or more", () => {
   expect(passwordVerifier("Mypassword1234")).toEqual("Password accepted");
   expect(passwordVerifier("1234")).toEqual("Password rejected");
 });
+test("Upper case letter", () => {
+  expect(passwordVerifier("12345678")).toEqual("Password rejected");
+  expect(passwordVerifier("12345Y78")).toEqual("Password accepted");
+});
+
+test("Password has a number", () => {
+  expect(passwordVerifier("ABCDEFGHIJ1")).toEqual("Password accepted");
+  expect(passwordVerifier("ABCDEFGHIJ")).toEqual("Password rejected");
+});
