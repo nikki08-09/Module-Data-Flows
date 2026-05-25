3ed0a686ddfb7b0654ad6751181be1d9ba695808
nikki08-09 <nikita.belgium@gmail.com>
2026-05-25 20:36:34 +0200

    Added the test for other numbers in the set of natural numbers

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -7,6 +7,65 @@ function convertToOldRoman(n) {
   else if (n === "100") return "C";
   else if (n === "500") return "D";
   else if (n === "1000") return "M";
+  else {
+    let number = Number(n);
+    let numerals = {};
+    while (number != 0) {
+      if (number >= 1000) {
+        number = number / 1000;
+        numerals.M = Math.trunc(number);
+        number = number * 1000 - 1000 * numerals.M;
+      } else if (number >= 500 && number < 1000) {
+        number = number / 500;
+        numerals.D = Math.trunc(number);
+        number = number * 500 - 500 * numerals.D;
+      } else if (number >= 100 && number < 500) {
+        number = number / 100;
+        numerals.C = Math.trunc(number);
+        number = number * 100 - 100 * numerals.C;
+      } else if (number >= 50 && number < 100) {
+        number = number / 50;
+        numerals.L = Math.trunc(number);
+        number = number * 50 - 50 * numerals.L;
+      } else if (number >= 10 && number < 50) {
+        number = number / 10;
+        numerals.X = Math.trunc(number);
+        number = number * 10 - numerals.X * 10;
+      } else if (number >= 5 && number < 10) {
+        number = number / 5;
+        numerals.V = Math.trunc(number);
+        number = number * 5 - numerals.V * 5;
+      } else {
+        numerals.I = Math.trunc(number);
+        number = 0;
+      }
+    }
+    let numeral = "";
+    for (num in numerals) {
+      if (num === "M") {
+        for (let i = 0; i < numerals.M; i++) numeral += "M";
+      }
+      if (num === "D") {
+        for (let i = 0; i < numerals.D; i++) numeral += "D";
+      }
+      if (num === "C") {
+        for (let i = 0; i < numerals.C; i++) numeral += "C";
+      }
+      if (num === "L") {
+        for (let i = 0; i < numerals.L; i++) numeral += "L";
+      }
+      if (num === "X") {
+        for (let i = 0; i < numerals.X; i++) numeral += "X";
+      }
+      if (num === "V") {
+        for (let i = 0; i < numerals.V; i++) numeral += "V";
+      }
+      if (num === "I") {
+        for (let i = 0; i < numerals.I; i++) numeral += "I";
+      }
+    }
+    return numeral;
+  }
 }
 
 module.exports = convertToOldRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -28,3 +28,11 @@ test("returns D if passed 1 as an argument", function () {
 test("returns M if passed 1 as an argument", function () {
   expect(convertToOldRoman("1000")).toEqual("M");
 });
+test("Test for the rest", () => {
+  expect(convertToOldRoman("1")).toEqual("I");
+  expect(convertToOldRoman("3")).toEqual("III");
+  expect(convertToOldRoman("7")).toEqual("VII");
+  expect(convertToOldRoman("15")).toEqual("XV");
+  expect(convertToOldRoman("18")).toEqual("XVIII");
+  expect(convertToOldRoman("22")).toEqual("XXII");
+});
