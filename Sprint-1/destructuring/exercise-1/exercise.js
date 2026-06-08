29413d4242be010c49eb5565ae65119c0e7c06dc
nikki08-09 <nikita.belgium@gmail.com>
2026-06-08 12:01:11 +0200

    Fix introduceYourself function parameter destructuring
    Removed default parameter value for introduceYourself function.

--- Sprint-1/destructuring/exercise-1/exercise.js
+++ Sprint-1/destructuring/exercise-1/exercise.js
@@ -5,7 +5,7 @@ const personOne = {
 };
 // Update the parameter to this function to make it work.
 // Don't change anything else.
-function introduceYourself({ name, age, favouriteFood } = personOne) {
+function introduceYourself({ name, age, favouriteFood }) {
   console.log(
     `Hello, my name is ${name}. I am ${age} years old and my favourite food is ${favouriteFood}.`
   );
